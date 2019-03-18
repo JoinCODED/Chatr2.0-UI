f9732c2d6bd2d5425f01ea809c59bf3e57c16dd5
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-03-18 11:48:42 +0300

    Hide user icon and -Add new channel button- when user loged out

--- src/components/Navigation/SideNav.js
+++ src/components/Navigation/SideNav.js
@@ -16,34 +16,38 @@ class SideNav extends React.Component {
     let user = this.props.user;
     let chs = this.props.channels;
 
-    console.log("RNDER SIDE NAV");
-    console.log("zerodebug => Channels: ", chs);
     const channelLinks = chs.map(channel => (
       <ChannelNavLink key={channel.name} channel={channel} />
     ));
-    console.log(user);
-    return (
-      <div>
-        <div className="row">
-          <div className="col-1">
-            <FontAwesomeIcon icon={faUser} />
+
+    if (user) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-1">
+              <FontAwesomeIcon icon={faUser} />
+            </div>
+            <div className="col-11">{user ? `Hi ${user.username}` : ""}</div>
           </div>
-          <div className="col-11">{user ? `Hi ${user.username}` : ""}</div>
-        </div>
-        <div className="row my-2">
-          <div className="col-1">
-            <FontAwesomeIcon icon={faPlus} />
+          <div className="row my-2">
+            <div className="col-1">
+              <FontAwesomeIcon icon={faPlus} />
+            </div>
+            <div className="col-11">
+              <Link to={user ? "/createChannel" : "/login"}>Add a channel</Link>
+            </div>
           </div>
-          <div className="col-11">
-            <Link to={user ? "/createChannel" : "/login"}>Add a channel</Link>
+          <hr />
+          <div
+            className="col-12 my-4 channels-board"
+            style={{ maxHeight: 500 }}
+          >
+            {user ? channelLinks : <div />}
           </div>
         </div>
-        <hr />
-        <div className="col-12 my-4 channels-board" style={{ maxHeight: 500 }}>
-          {user ? channelLinks : <div />}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div />;
   }
 }
 
