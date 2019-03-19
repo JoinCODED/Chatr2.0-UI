e556ab62d32b1c37e4582f71a6293e125409cdb1
Sultan Alshahrani <sultanalshahrani@Sultans-MacBook-Pro.local>
2019-03-19 11:10:51 +0300

    design

--- src/store/actions/channels.js
+++ src/store/actions/channels.js
@@ -2,21 +2,20 @@ import * as actionTypes from "./actionTypes";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://api-chatr.herokuapp.com/"
+  baseURL: "https://api-chatr.herokuapp.com/"
 });
 
 export const fetchChannels = () => {
-    return async dispatch => {
-        try {
-            const res = await instance.get("/channels/");
-            const channels = res.data;
-            dispatch({
-                type: actionTypes.FETCH_CHANNELS,
-                payload: channels
-            })
-        } catch (err) {
-        }
-    };
+  return async dispatch => {
+    try {
+      const res = await instance.get("/channels/");
+      const channels = res.data;
+      dispatch({
+        type: actionTypes.FETCH_CHANNELS,
+        payload: channels
+      });
+    } catch (err) {}
+  };
 };
 
 // export const filterChannels = query => {
@@ -29,27 +28,17 @@ export const fetchChannels = () => {
 // };
 
 export const postChannel = (newChannel, closeModal) => {
-    return async dispatch => {
-        try {
-            const res = await instance.post("/channels/create/", newChannel);
-            const channel = res.data;
-            // dispatch(resetErrors());
-            dispatch({
-                type: actionTypes.POST_CHANNEL,
-                payload: channel
-            });
-            // dispatch(filterChannel(""));
-            closeModal();
-        } catch (err) {
-            // err.response.data
-            // Object.keys(err.response.data)
-            // Object.keys(err.response.data).map(
-            //     key => `${key}: ${err.response.data[key]}`
-            // )
-            dispatch({
-                // type: actionTypes.SET_ERRORS,
-                //payload: err.response.data
-            });
-        }
-    };
+  return async dispatch => {
+    try {
+      const res = await instance.post("/channels/create/", newChannel);
+      const channel = res.data;
+      // dispatch(resetErrors());
+      dispatch({
+        type: actionTypes.POST_CHANNEL,
+        payload: channel
+      });
+      // dispatch(filterChannel(""));
+      closeModal();
+    } catch (err) {}
+  };
 };
